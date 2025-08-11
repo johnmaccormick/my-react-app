920c2babe6c1a75efdf3f0ec95969fa4c5053109
MacCormick <jmac@dickinson.edu>
2025-08-11 15:54:56 -0400

    Switching to direct deployment from Vite without GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
+// Change this to match your repository name exactly
+const repoName = 'my-react-app'
+
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === 'build' ? '/my-react-app/' : '/',
-})
+  base: command === 'build' ? `/${repoName}/` : '/',
+}))
+
